f626f967649d03990a6f68ee2eb030b67f8dbb54
dvdek12 <przemekdudek321@wp.pl>
2022-07-21 08:33:38 +0200

    usunieto zbedne znaki zmieniono kolejnosc warunkow w kalsie fizzbuzz

--- fizzbuzz2/Program.cs
+++ fizzbuzz2/Program.cs
@@ -1,31 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-for (int i = 0; i <= 100; i++)
+﻿for (int i = 0; i <= 100; i++)
 {
-    Console.WriteLine(new FizzBuzz(i).value);
+    Console.WriteLine(new FizzBuzz(i).Value);
 }
-
-
 
 class FizzBuzz
 {
-    public string value { get; set; }
+    public string Value { get; set; }
     public FizzBuzz(int value)
     {
-        if(value % 3 == 0)
+        if(value % 3 == 0 && value % 5 == 0)
         {
-            this.value = "Fizz";
-        }else if(value % 5 == 0)
+            this.Value = "FizzBuzz";
+        }
+        else if(value % 5 == 0)
         {
-            this.value = "Buzz";
-        } else if(value % 3 == 0 && value % 5 == 0)
+            this.Value = "Buzz";
+        } 
+        else if(value % 3 == 0)
         {
-            this.value = "FizzBuzz";
+            this.Value = "Fizz";
         }
         else
         {
-            this.value = value.ToString();
+            this.Value = value.ToString();
         }
     }
 }
